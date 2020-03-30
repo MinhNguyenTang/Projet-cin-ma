9bab5b5c299c91ecb3305572faefb39dba4370dc
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2020-03-30 11:26:38 +0200

    Add files via upload

--- projet_cinema.sql
+++ projet_cinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 02 mars 2020 à 11:25
+-- Généré le :  lun. 30 mars 2020 à 08:16
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -25,14 +25,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id_admin` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `identifiant` varchar(255) COLLATE utf8_bin NOT NULL,
+  `mdp` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `identifiant`, `mdp`) VALUES
+(1, 'Admin01', 'ff5a42b9ef351a1ade91fd6d33f7de37'),
+(3, 'Admin02', '7cd148a246ecd53b555aa1b7f050f28b');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `client_reservation`
 --
 
 DROP TABLE IF EXISTS `client_reservation`;
 CREATE TABLE IF NOT EXISTS `client_reservation` (
+  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `nb_place` int(11) NOT NULL,
   `code_reduction` int(11) NOT NULL,
-  `paiement` int(11) NOT NULL
+  `paiement` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -46,6 +71,21 @@ CREATE TABLE IF NOT EXISTS `connexion` (
   `id` smallint(6) UNSIGNED NOT NULL AUTO_INCREMENT,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `mdp` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `message` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
