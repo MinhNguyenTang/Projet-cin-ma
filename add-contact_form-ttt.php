e2dbd17313eb22488fe9763c4711712fce69f839
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2020-03-30 10:00:35 +0200

    Add files via upload

--- add-contact_form-ttt.php
+++ add-contact_form-ttt.php
@@ -3,6 +3,15 @@
 if(empty($_POST['nom']) || empty($_POST['email']) || empty($_POST['message'])){
   $error = 'Veuillez remplir les champs';
 }
+if(empty($_POST['nom'])){
+  $error = 'Veuillez indiquer votre nom'
+}
+if(empty($_POST['email'])){
+  $error = 'Veuillez indiquer votre adresse e-mail';
+}
+if(empty($_POST['message'])){
+  $error = 'Veuillez indiquer un message';
+}
 else{
   $contact = new MembreContact([
     'nom' => $_POST['nom'],
@@ -12,7 +21,7 @@ else{
 
   $bdd = new PDO('mysql:host=localhost;dbname=projet_cinema;charset=utf8', 'root', '');
   $manager = new ManagerContact($bdd);
-  $manager->getAdd($form); 
+  $manager->getAdd($form);
 }
 
  ?>
